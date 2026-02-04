1efed3146db4411dce4de65f7a13e1e03e6eadb4
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-02-05 04:37:58 +0800

    update

--- resources/views/app/pages/input.blade.php
+++ resources/views/app/pages/input.blade.php
@@ -89,7 +89,7 @@
             <input name="item_code" value="{{ $newItemCode }}"
               class="mt-1 w-full rounded-xl bg-white border border-slate-200 px-3 py-2 outline-none
                     focus:border-[#118EEA] focus:ring-2 focus:ring-[#118EEA]/20"
-              placeholder="contoh: WIRE_GALV">
+              placeholder="contoh: WIRE_DBS">
             @error('item_code') <div class="mt-1 text-xs text-red-600">{{ $message }}</div> @enderror
           </div>
         </div>
